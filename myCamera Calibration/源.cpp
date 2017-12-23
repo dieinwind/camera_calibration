c107941e690c4150908b587302e86d29ab3bb3df
dieinwind <dieinwind@gmail.com>
2017-12-23 17:58:13 +0800

    添加资源文件

---
+++ myCamera Calibration/源.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdio.h>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+//ÆÁ±ÎÄÚ´æÐ¹Â©¾¯¸æ
+#ifndef _CRT_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS
+#endif
+
+using namespace std;
+using namespace cv;
+
+
+
+int main()
+{
+	int mode = 0;
+	
+	switch (mode)
+	{
+
+	}
+}
